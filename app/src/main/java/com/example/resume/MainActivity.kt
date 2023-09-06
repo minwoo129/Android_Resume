95e495f44c27d0cbe743b04710286b9b3c711e05
minwoo129 <key0129mw@naver.com>
2023-09-06 22:34:21 +0900

    이력서 앱 Scaffold 적용

--- app/src/main/java/com/example/resume/MainActivity.kt
+++ app/src/main/java/com/example/resume/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.resume
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,29 +22,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             ResumeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyResume()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyResume() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Android Dev Resume") }
+            )
+        }
+    ) {paddingValues ->
+        MyResumeContent(paddingValues)
+    }
+}
+
+@Composable
+fun MyResumeContent(paddingValues: PaddingValues) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ResumeTheme {
-        Greeting("Android")
+        MyResume()
     }
 }
