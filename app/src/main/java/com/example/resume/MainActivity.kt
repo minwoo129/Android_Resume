266aeafd35ce2eaf9f20793aa0c15877e23b7531
minwoo129 <key0129mw@naver.com>
2023-09-06 23:10:57 +0900

    전화걸기, 이메일 보내기 버튼 배치

--- app/src/main/java/com/example/resume/MainActivity.kt
+++ app/src/main/java/com/example/resume/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.resume
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,13 +10,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -25,6 +30,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,6 +66,9 @@ fun MyResume() {
 
 @Composable
 fun MyResumeContent(paddingValues: PaddingValues) {
+
+    val context = LocalContext.current;
+
     Card(
         modifier = Modifier
             .fillMaxSize()
@@ -90,6 +100,60 @@ fun MyResumeContent(paddingValues: PaddingValues) {
                 fontSize = 20.sp,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "안녕하십니까. 저는 모바일 개발자입니다. 지금은 React Native를 사용하고 있지만, 안드로이드로 모바일에 입문하였습니다. JetPack Compose 너무 놀랍습니다.",
+                fontSize = 15.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "어느 누구와 달리 위의 자기소개는 진심입니다.",
+                fontSize = 10.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                color = Color.Red
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Divider(color = Color.Gray, thickness = 1.dp)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "휴대폰 번호 : 010-1234-5678",
+                modifier = Modifier.padding(5.dp)
+            )
+
+            Text(
+                text = "이메일 : example@gmail.com",
+                modifier = Modifier.padding(5.dp)
+            )
+
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:01012345678"));
+                    context.startActivity(intent);
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "전화걸기")
+            }
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_SENDTO).apply {
+                        data = Uri.parse("mailto:example@gmail.com")
+                    }
+                    context.startActivity(intent);
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "이메일 보내기")
+            }
         }
     }
 }
