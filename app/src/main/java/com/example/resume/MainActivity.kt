f41deefdc9adb8744541e16ce4507d53c00ea028
minwoo129 <key0129mw@naver.com>
2023-09-06 22:55:23 +0900

    컨텐트 요소 배치 중간 저장

--- app/src/main/java/com/example/resume/MainActivity.kt
+++ app/src/main/java/com/example/resume/MainActivity.kt
@@ -3,8 +3,18 @@ package com.example.resume
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -12,8 +22,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.resume.ui.theme.ResumeTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +59,39 @@ fun MyResume() {
 
 @Composable
 fun MyResumeContent(paddingValues: PaddingValues) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .padding(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        shape = RoundedCornerShape(30.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile2),
+                contentDescription = "profile",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .align(Alignment.CenterHorizontally)
+            )
 
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = "자기소개",
+                fontSize = 20.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
